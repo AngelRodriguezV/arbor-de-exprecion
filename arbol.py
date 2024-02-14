d5857df021ff5ddc174c682d079477882a76d658
Rodríguez Venegas Ángel de Jesús <43628553+AngelRodriguezV@users.noreply.github.com>
2024-02-14 08:14:08 -0600

    Algoritmo copy arbol

--- arbol.py
+++ arbol.py
@@ -22,3 +22,29 @@ class Arbol:
             aux = self.raiz
             self.raiz = nodo
             self.raiz.left = aux
+
+    def isOperador(caracter):
+        return caracter in "+-*/"
+
+    def crearArbol(self, cadena):
+        # Recorre la cadena
+        for i in range(0,len(cadena)):
+            # Mientras el caracter diferente de nulo
+            if cadena[i] != None:
+                # Leer caracter
+                caracter = cadena[i]
+                # Si es parentesis pasar al siguiente caracter
+                if caracter == "(" or caracter == ")":
+                    self.numero_parentesis += 1
+                    continue
+                # Crear un nodoo nuevo que contenga ese caracter
+                nodo = Nodo(data=caracter)
+
+                if self.isOperador(caracter):
+                    print(nodo.data + "  Es operador")
+                else:
+                    nodo.type = 'hoja'
+                    if self.arbol.isEmpty():
+                        self.arbol.raiz = nodo
+                    else:
+                        pass
