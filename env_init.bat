7889db766edef4fe1d67ae1a33d303ea1c57b914
Rodríguez Venegas Ángel de Jesús <43628553+AngelRodriguezV@users.noreply.github.com>
2024-02-23 08:07:52 -0600

    update paquetes requeridos

--- env_init.bat
+++ env_init.bat
@@ -10,4 +10,5 @@ echo Entorno virtual activado
 :: Instalar las dependencias
 py -m pip install --upgrade pip
 pip install pygame
-echo Se Instalo Pygame
+pip install pygame_gui
+echo Se Instalo Pygame y PyGame_GUI
